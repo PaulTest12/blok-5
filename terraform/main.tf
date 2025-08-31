13aaaaeeea018fe701f896f02519fa65bb9ffd30
PaulTest12 <mckfc30@gmail.com>
2025-08-31 19:55:06 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,19 +9,18 @@ terraform {
 
 provider "docker" {}
 
-resource "docker_image" "sshd" {
-  name = "rastasheep/ubuntu-sshd:latest"
+resource "docker_image" "ubuntu_sshd" {
+  name = "ubuntu:22.04"
 }
 
 resource "docker_container" "vm" {
   name  = "vm1"
-  image = docker_image.sshd.name
-
+  image = docker_image.ubuntu_sshd.latest
+  tty   = true
   ports {
     internal = 22
     external = 2222
   }
-
   ports {
     internal = 80
     external = 8080
@@ -29,10 +28,6 @@ resource "docker_container" "vm" {
 }
 
 resource "local_file" "ansible_inventory" {
-  content = templatefile("${path.module}/inventory.tpl", {
-    ip   = "127.0.0.1"
-    port = 2222
-  })
-
+  content  = templatefile("${path.module}/inventory.tpl", { ip = "127.0.0.1", port = 2222 })
   filename = "${path.module}/../ansible/hosts"
 }
